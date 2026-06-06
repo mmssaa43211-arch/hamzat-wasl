6a421f41aeb3acdbde49f5a977438bb303853796
mmssaa43211-arch <mmssaa43211@gmail.com>
2026-06-06 16:47:54 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>مرحباً بك في منصة همزة وصل</h1>
+      <p>الموقع قيد التطوير والعمل جارٍ على إطلاقه بالكامل.</p>
+    </div>
+  )
+}
